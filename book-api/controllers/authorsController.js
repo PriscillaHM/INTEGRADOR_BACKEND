8edc0a91191de28068365ebfcdab19cf6592acc3
PriscillaHM <al20760537@ite.edu.mx>
2025-03-19 19:55:59 -0700

    controlador autores

--- book-api/controllers/authorsController.js
+++ book-api/controllers/authorsController.js
@@ -1,13 +1,26 @@
-const authorsModel = require('../models/authorsModel'); // Importamos el modelo de autores
-const authorsView = require('../views/authorsView'); // Importamos la vista
+// Importamos el modelo de autores, que contiene la lógica de datos
+const authorsModel = require('../models/authorsModel'); 
+// Importamos la vista, que contiene las funciones para formatear las respuestas
+const authorsView = require('../views/authorsView'); 
 
-// Obtiene la lista de autores y la formatea antes de enviarla
+/**
+ * Obtiene la lista de autores desde el modelo y la formatea antes de enviarla.
+ * @returns {string} Lista de autores formateada o un mensaje si no hay autores disponibles.
+ */
 const getAuthors = () => {
     return authorsView.formatAuthorsResponse(authorsModel.getAuthors());
 };
 
+/**
+ * Agrega un nuevo autor y retorna un mensaje formateado con los datos del autor agregado.
+ * @param {Object} author - Objeto con los datos del autor a agregar.
+ * @param {string} author.name - Nombre del autor.
+ * @param {string} author.country - País del autor.
+ * @returns {string} Mensaje confirmando la adición del autor con su información formateada.
+ */
 const addAuthors = (author) => {
-    return ` The author has been added:\n${authorsView.formatAuthorAddedResponse([authorsModel.addAuthors(author)])}`;
+    return `The author has been added:\n${authorsView.formatAuthorAddedResponse([authorsModel.addAuthors(author)])}`;
 };
-// Exportamos las funciones para usarse en el servidor
+
+// Exportamos las funciones para que puedan ser utilizadas en otros módulos del servidor
 module.exports = { getAuthors, addAuthors };
