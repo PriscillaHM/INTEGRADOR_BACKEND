6648af47854d80380876c18255edc8eb032d70e5
PriscillaHM <al20760537@ite.edu.mx>
2025-03-19 20:59:38 -0700

    documentacion de cliente

--- book-api/client.js
+++ book-api/client.js
@@ -1,22 +1,36 @@
+//Importamos el modulo net
 const net = require('net');
+//Importamos el modulo readline
 const readline =require('readline');
 
+/**
+ * Creamos una interfaz para leer la entrada del usuario desde la terminal
+ */
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
 
+/**
+ * Configuración de la conexión con el servidor
+ */
 const options = {
     port:8080,
     host : '127.0.0.1'
 }
 
+/**
+ * Establecemos la conexión con el servidor TCP
+ */
 const client = net.createConnection(options, () => {
     console.log('Satisfactory connection established');
     // Llamada inicial para preguntar al usuario
     promptForCommand();
 });
 
+/**
+ * unción para solicitar un comando al usuario y enviarlo al servidor.
+ */
 function promptForCommand() {
     rl.question('Write a command (for example: "GET BOOK" or "ADD BOOK {"id": 1, "title": "El Quijote", "author_id": 1, "publisher_id": 1, "year": 1605, "genre": "Novela" }")\n', (command) => {
         client.write(command); // Enviamos el comando al servidor
@@ -31,14 +45,18 @@ function promptForCommand() {
     });
 }
 
-// Evento para manejar el cierre de la conexión
 
+/**
+ * Evento que se ejecuta cuando la conexión con el servidor se cierra
+ */
 client.on('close',()=>{
     console.log('Communication has ended');
     rl.close(); // Cerramos la interfaz de readline
 })
 
-// Manejo de errores en la conexión
+/**
+ * Evento para manejar errores en la conexión
+ */
 client.on('error', (err) => {
     console.error('Error with connection: ' + err.message);
     rl.close(); // Cerramos la interfaz de readline en caso de error
